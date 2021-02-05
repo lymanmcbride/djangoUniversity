f1ebd1ee7a360bddc0fe24abf4342948ef2576bf
Lyman McBride <68021552+lymanmcbride@users.noreply.github.com>
2021-02-05 16:38:36 -0700

    complete django challenge

---
+++ Classes/Classes/classApp/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import DjangoClasses
+# Register your models here.
+
+# necessary. That's all I know.
+admin.site.register(DjangoClasses)
---
+++ Classes/Classes/classApp/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+# This class sets up the objects that are mapped to the database. You have to make migrations when you edit this
+class DjangoClasses(models.Model):
+    title = models.CharField(max_length=50)
+    course_number = models.IntegerField()
+    instructor_name = models.CharField(max_length=50)
+    duration = models.FloatField(max_length=4)
+
+    objects = models.Manager()
+
+    # This is like a __repr__, it determines what to show you when you call the object itself
+    def __str__(self):
+        return "{}".format(self.title)
